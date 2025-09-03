9107537d0e5fe28193d2765c30f6cdb13e0c4a02
Owinogoddie <owinogoddie@gmail.com>
2025-09-03 14:01:48 +0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaModule } from 'prisma/prisma.module';
+// import { AuthModule } from './auth/auth.module';
+// import { FarmersModule } from './farmers/farmers.module';
+// import { CropsModule } from './crops/crops.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    // AuthModule,
+    // FarmersModule,
+    // CropsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
